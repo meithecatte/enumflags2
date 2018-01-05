dfd273fb782032bbf7899f104e6b440f40d2b110
Maik Klein <maikklein@googlemail.com>
2018-01-06 00:02:05 +0100

    Add test_suite

--- test_suite/tests/bitflag_tests.rs
+++ test_suite/tests/bitflag_tests.rs
@@ -15,5 +15,37 @@ enum Test {
 
 #[test]
 fn test_foo() {
-    panic!()
+    use enumflags::BitFlags;
+    assert_eq!(
+        BitFlags::<Test>::all(),
+        Test::A | Test::B | Test::C | Test::D
+    );
+    assert_eq!(BitFlags::<Test>::all() & Test::A, Test::A);
+    assert_eq!(!Test::A, Test::B | Test::C | Test::D);
+    assert_eq!((Test::A | Test::C) ^ (Test::C | Test::B), Test::A | Test::B);
+    assert_eq!(BitFlags::<Test>::from_bits_truncate(4), Test::C);
+    assert_eq!(BitFlags::<Test>::from_bits_truncate(5), Test::A | Test::C);
+    assert_eq!(
+        BitFlags::<Test>::from_bits_truncate(16),
+        BitFlags::<Test>::empty()
+    );
+    assert_eq!(BitFlags::<Test>::from_bits_truncate(17), Test::A);
+    assert_eq!(BitFlags::<Test>::from_bits(17), None);
+    assert_eq!(
+        BitFlags::<Test>::from_bits(15),
+        Some(BitFlags::<Test>::all())
+    );
+    {
+        let mut b = (Test::A | Test::B);
+        b.insert(Test::C);
+        assert_eq!(b, Test::A | Test::B | Test::C);
+    }
+    assert!((Test::A | Test::B).intersects(Test::B));
+    assert!(!(Test::A | Test::B).intersects(Test::C));
+    assert!((Test::A | Test::B | Test::C).contains(Test::A | Test::B));
+    assert!(!(Test::A | Test::B | Test::C).contains(Test::A | Test::D));
+    assert_eq!(!(Test::A | Test::B), Test::C | Test::D);
+    assert_eq!((Test::A | Test::B).bits(), 3);
+    assert_eq!((Test::A | Test::B).not().bits(), 12);
+    assert_eq!(BitFlags::<Test>::all().bits(), 15);
 }
