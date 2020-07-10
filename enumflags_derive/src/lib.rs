abbffdc57df135d9334be7852dd0739c6adba6be
Jakub Kądziołka <kuba@kadziolka.net>
2020-07-10 14:59:52 +0200

    Don't show generated assertions in the documentation

--- enumflags_derive/src/lib.rs
+++ enumflags_derive/src/lib.rs
@@ -135,6 +135,7 @@ fn verify_flag_values<'a>(
                     Span::call_site()); // call_site because def_site is unstable
                 // adapted from static-assertions-rs by nvzqz (MIT/Apache-2.0)
                 deferred_checks.push(quote_spanned!(variant.span() =>
+                    #[doc(hidden)]
                     const #assertion_name: fn() = || {
                         ::enumflags2::_internal::assert_exactly_one_bit_set::<[(); (
                             (#type_name::#variant_name as u64).wrapping_sub(1) &
