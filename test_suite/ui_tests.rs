ebffeddc83de38bf51cc6a2e9c60c64a98348269
Jakub Bandola <xxxkubikx@gmail.com>
2021-02-23 21:52:37 +0100

    Implement UI tests without Unix-specific APIs

--- test_suite/ui_tests.rs
+++ test_suite/ui_tests.rs
@@ -1,14 +1,14 @@
 use glob::glob;
-use std::os::unix::ffi::OsStrExt;
 
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
     for test in glob("ui/*.rs").unwrap() {
         let path = test.unwrap();
-        match path.as_os_str().as_bytes() {
-            b"ui/must_use_warning.rs" => t.pass(path),
-            _ => t.compile_fail(path),
+        if path == std::path::Path::new("ui/must_use_warning.rs") {
+            t.pass(path)
+        } else {
+            t.compile_fail(path)
         }
     }
 }
