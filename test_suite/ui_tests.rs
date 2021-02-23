04c35de5e62d6f561b59d2d9bad7ed95610d84cc
Jakub Bandola <xxxkubikx@gmail.com>
2021-02-23 16:06:28 +0100

    Hopefully fix pipeline build

--- test_suite/ui_tests.rs
+++ test_suite/ui_tests.rs
@@ -12,3 +12,18 @@ fn ui() {
         }
     }
 }
+
+#[cfg(windows)]
+#[test]
+fn ui() {
+    use glob::glob;
+    let t = trybuild::TestCases::new();
+    for test in glob("ui/*.rs").unwrap() {
+        let path = test.unwrap();
+        if path == std::path::Path::new("ui/must_use_warning.rs") {
+            t.pass(path)
+        } else {
+            t.compile_fail(path)
+        }
+    }
+}
